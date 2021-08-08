55c1beff8c37379b1bfb225a1bf6dabd5b1db3bf
ahSOLO <junmenghan@gmail.com>
2021-08-08 12:44:55 -0700

    add second solution for BST search kata

--- AE/Easy/find_closest_value_in_bst.cs
+++ AE/Easy/find_closest_value_in_bst.cs
@@ -2,6 +2,7 @@ using System;
 
 // First Attempt
 
+/*
 public class Program {
 	public static int FindClosestValueInBst(BST tree, int target) {
 		int diff = Math.Abs(tree.value - target);
@@ -34,6 +35,35 @@ public class Program {
 		}
 	}
 }
+*/
 
 // Most efficient solution
 
+public class Program {
+	public static int FindClosestValueInBst(BST tree, int target) {
+		var node = tree;
+		var closest = tree.value;
+		while(node != null)
+		{
+			if (Math.Abs(node.value - target) < Math.Abs(closest - target))
+				closest = node.value;
+			if (target < node.value)
+				node = node.left;
+			else if (target > node.value)
+				node = node.right;
+			else 
+				break;
+		}
+		return closest;
+	}
+
+	public class BST {
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+}
