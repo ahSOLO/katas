c3aa93fdc21753e224ec57d18adbe547e33a8eb8
ahSOLO <junmenghan@gmail.com>
2023-05-28 20:38:55 -0700

    refactor min number of coins for change

--- AE/Medium/min_number_of_coins_for_change.cs
+++ AE/Medium/min_number_of_coins_for_change.cs
@@ -2,28 +2,23 @@ using System;
 
 public class Program {
 	public static int MinNumberOfCoinsForChange(int n, int[] denoms) {
-		int[] numOfCoins = new int[n + 1];
-        Array.Fill(numOfCoins, Int32.MaxValue);
-        numOfCoins[0] = 0;
-        int toCompare = 0;
+		int[] minChange = new int[n+1];
+        Array.Fill(minChange, Int32.MaxValue);
+        minChange[0] = 0;
         foreach (int denom in denoms)
         {
-            for (int amount = 0; amount < numOfCoins.Length; amount++)
+            for (int i = 1; i <= n; i++)
             {
-                if (denom <= amount)
+                if (i >= denom)
                 {
-                    if (numOfCoins[amount - denom] == Int32.MaxValue)
+                    if (minChange[i - denom] != Int32.MaxValue)
                     {
-                        toCompare = Int32.MaxValue;
+                        minChange[i] = Math.Min(minChange[i], 1 + minChange[i - denom]);
                     }
-                    else
-                    {
-                        toCompare = 1 + numOfCoins[amount - denom];
-                    }
-                    numOfCoins[amount] = Math.Min(numOfCoins[amount], toCompare);
                 }
             }
         }
-		return numOfCoins[n] != Int32.MaxValue ? numOfCoins[n] : -1;
+        
+		return minChange[n] != Int32.MaxValue ? minChange[n] : -1;
 	}
 }
