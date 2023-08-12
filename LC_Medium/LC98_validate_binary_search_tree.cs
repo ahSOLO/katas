89fccbc867cd470f97a0885c8141a27398b529b5
ahSOLO <junmenghan@gmail.com>
2023-08-12 11:15:13 -0700

    add validate binary search tree kata

---
+++ LC_Medium/LC98_validate_binary_search_tree.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsValidBST(TreeNode root, int? min = null, int? max = null) {
+        if (root == null)
+        {
+            return true;
+        }
+        if (min != null && min >= root.val)
+        {
+            return false;
+        }
+        if (max != null && max <= root.val)
+        {
+            return false;
+        }
+        return IsValidBST(root.left, min, root.val) && IsValidBST(root.right, root.val, max);
+    }
+}
