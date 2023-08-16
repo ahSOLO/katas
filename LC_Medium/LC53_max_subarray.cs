90a2d1a0a487788fc0dbc4c2ceec5d561e88c51a
ahSOLO <junmenghan@gmail.com>
2023-08-15 17:02:10 -0700

    add LC53 max subarray

---
+++ LC_Medium/LC53_max_subarray.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int curSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            curSum += nums[i];
+            maxSum = Math.Max(maxSum, curSum);
+            if (curSum < 0)
+            {
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
